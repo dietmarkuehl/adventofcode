857351aa2e7627a78684fb65202f8490df43c579
Dietmar Kühl <dietmar.kuehl@me.com>
2022-12-06 05:31:06 +0000

    added today's puzzle

---
+++ 2022/ad11.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <iterator>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+int main() {
+    std::size_t size(4);
+    for (std::string line; std::getline(std::cin, line); ) {
+        for (std::size_t i{}; i < line.size() - size; ++i) {
+	    std::string tmp = line.substr(i, size);
+	    std::sort(tmp.begin(), tmp.end());
+	    if (std::unique(tmp.begin(), tmp.end()) == tmp.end()) {
+	        std::cout << (i + size) << '\n';
+		break;
+	    }
+	}
+    }
+    std::cout << "---\n";
+}
