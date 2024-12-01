0a383a24882acec6372e279080578af355fd7956
Dietmar Kühl <dietmar.kuehl@me.com>
2024-12-01 08:54:12 +0000

    adding 2024-12-01

---
+++ 2024/ad01-2.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <iterator>
+#include <ranges>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <cinttypes>
+
+int main() {
+    std::vector<std::int64_t> l0, l1;
+    for (int v0, v1; std::cin >> v0 >> v1; ) {
+        l0.push_back(v0);
+        l1.push_back(v1);
+    }
+    std::ranges::sort(l0);
+    std::ranges::sort(l1);
+    std::int64_t sum{};
+    for (std::size_t i{}; i != l0.size(); ++i) {
+        sum += l0[i] * std::ranges::count(l1, l0[i]);
+    }
+    std::cout << sum << "\n";
+}
