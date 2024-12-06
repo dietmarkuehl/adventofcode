ee38b477af6dfd93a852cf9c5c696169aff874f3
Dietmar Kühl <dietmar.kuehl@me.com>
2024-12-06 18:17:46 +0000

    added trying obstacles only when the guard goes there

--- 2024/ad06-2.cpp
+++ 2024/ad06-2.cpp
@@ -8,36 +8,32 @@
 
 int main() {
     std::vector<std::string> map{std::istream_iterator<std::string>(std::cin), {}};
-    //auto print{[&map]{
-    //    std::copy(map.begin(), map.end(), std::ostream_iterator<std::string>(std::cout, "\n"));
-    //}};
     int x{}, y{};
     for (; y < (int)map.size(); ++y)
         if ((x = map[y].find('^')) != int(std::string::npos))
             break;
 
-    auto walk{[](int x, int y, auto map)mutable{
-            auto lprint{[&map]{
-                for (std::size_t i{}; i != map[0].size(); ++i)
-                {
-                    for (std::size_t j{}; j != map.size(); ++j)
-                        switch (map[i][j]) {
-                        default:
-                            std::cout << "0123456789ABCDEF"[(int)map[i][j]];
-                            break;
-                        case '.':
-                        case '#':
-                        case '^':
-                            std::cout << map[i][j];
-                            break;
-                        }
-                     std::cout << "\n";
-                }
-                std::cout << "\n";
-                return true;
-            }};
-        struct direction { int x; int y; };
-        direction dirs[] = { {0, 1}, {-1, 0}, {0, -1}, {1, 0} };
+    struct direction { int x; int y; };
+    direction dirs[] = { {0, 1}, {-1, 0}, {0, -1}, {1, 0} };
+    auto marks(map);
+    auto mark([&dirs, &marks, x, y]() mutable{
+        int current{2};
+        while (0 <= x && x < int(marks[0].size()) && 0 <= y && y < int(marks.size()))
+        {
+            marks[y][x] = 'X';
+            int nx = x + dirs[current].x;
+            int ny = y + dirs[current].y;
+            if (marks[ny][nx] == '#')
+                current = (current + 1) % 4;
+            else {
+                x = nx;
+                y = ny;
+            }
+        }
+    });
+    mark();
+
+    auto walk{[&dirs](int x, int y, auto map)mutable{
         int current{2};
         while (0 <= x && x < int(map[0].size()) && 0 <= y && y < int(map.size()))
         {
@@ -45,7 +41,6 @@ int main() {
                 map[y][x] = 1 << current;
             }
             else if (map[y][x] & (1 << current)) {
-                 //lprint();
                  return true;
             }
             else {
@@ -60,13 +55,12 @@ int main() {
                 y = ny;
             }
         }
-        //lprint();
         return false;
     }};
     int count {};
     for (int yy{}; yy < (int)map.size(); ++yy)
         for (int xx{}; xx < (int)map.size(); ++xx)
-            if (map[yy][xx] == '.') {
+            if (marks[yy][xx] == 'X' && map[yy][xx] == '.') {
                 map[yy][xx] = '#';
                 count += walk(x, y, map);
                 map[yy][xx] = '.';
